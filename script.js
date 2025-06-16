3a0f851bfab5c5a9de18121fe55737dd34853f23
wowskii <quentutu2@gmail.com>
2025-06-16 21:50:33 +0200

    fixing dark mode issues

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.getElementById('mode-toggle').addEventListener('click', function() {
     document.body.classList.toggle('dark-mode');
+    document.itemBox.classList.toggle('dark-mode');
 });
 
 //Dynamic lists
