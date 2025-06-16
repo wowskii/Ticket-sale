7a13246ebe82955344579fd91087a8bcf4c76ac4
wowskii <quentutu2@gmail.com>
2025-06-16 21:09:12 +0200

    Created a dynamic list for projects

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 document.getElementById('mode-toggle').addEventListener('click', function() {
     document.body.classList.toggle('dark-mode');
 });
+
+//Dynamic lists
+
+
+const itemInput = document.getElementById('itemInput');
+const addProjectButton = document.getElementById('addProjectButton');
+const dynamicList = document.getElementById('dynamicList');
+
+//Add item to the list
+function addItem() {
+    const itemText = itemInput.value.trim();
+
+    if (itemText) {
+        const li = document.createElement('li');
+        li.textContent = itemText;
+        dynamicList.appendChild(li);
+        itemInput.value = '';
+    }
+}
+
+addProjectButton.addEventListener('click', addItem);
